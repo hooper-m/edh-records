ef8215ff4faa8520f7b81b18bfd7630f93393bb2
Gramphics <hooperm.161@gmail.com>
2023-11-24 12:54:45 -0700

    delete legacy code :)

--- edh-stats.py
+++ edh-stats.py
@@ -213,7 +213,7 @@ class Game:
             decks_by_assists[assists].append(deck)
         for decks in reversed(decks_by_assists):
             if len(decks):
-                self._rankings.append(sorted(decks, key=lambda d: decks_by_turn_order.index(d)))
+                self._rankings.append(sorted(decks, key=decks_by_turn_order.index))
 
     def get_rankings(self):
         decks = []
@@ -325,12 +325,10 @@ def update_record_results(decks, games):
 
     last_game = games_as_list[-1]
 
-    s = sorted(filter(lambda d: d.played, decks.values()), key=lambda d: d.name)
-
-    for deck in s:
+    for deck in filter(lambda d: d.played, decks.values()):
         deck.calculate_metrics(decks)
 
-    _calculate_ranks(decks)
+    calculate_ranks(decks)
 
     for game in last_game[1]:
         wins_by_turn_order[len(game.decks)][game.decks.index(game.winner) + 1] += 1
@@ -350,7 +348,7 @@ def update_record_results(decks, games):
     for deck in decks.values():
         deck.calculate_metrics(decks)
 
-    _calculate_ranks(decks)
+    calculate_ranks(decks)
 
     return wins_by_turn_order
 
@@ -362,7 +360,7 @@ def by_speed(d):
     return d.get_avg_win_turn(), d.get_avg_elim_turn(), d.get_fastest_win(), d.get_fastest_elim(), -d.winrate
 
 
-def _calculate_ranks(decks):
+def calculate_ranks(decks):
     ranked = list(filter(lambda d: d.is_ranked(), decks.values()))
 
     for metric, ranking in {
@@ -431,186 +429,6 @@ def print_decks(decks, key):
     print()
 
 
-def foo(d):
-    before = parse_date(d)
-    with open('./data/edh.json', 'r',) as f:
-        record = json.load(f)
-    gs = record['games']
-
-    decks = {}
-    diffs = {}
-    prev_date = ''
-    wins_by_turn_order = {
-        3: Counter(),
-        4: Counter()
-    }
-
-    for g in gs:
-        date = g['date']
-        if parse_date(date) > before:
-            return decks, diffs
-        if date != prev_date:
-            diffs[date] = {}
-            prev_date = date
-        games = g['games']
-        for game in games:
-            winner_name = game['winner']
-            players = [p['deck'] for p in game['players']]
-            losers = [d['deck'] for d in game['players'] if d['deck'] != winner_name]
-
-            if parse_date(date) >= parse_date('2/2/2023'):
-                wins_by_turn_order[len(players)][players.index(winner_name) + 1] += 1
-
-            for deck_name in players:
-                if deck_name not in decks:
-                    decks[deck_name] = {
-                        'name': deck_name,
-                        'wins': 0,
-                        'played': 0,
-                        'score': 0,
-                        'matchups': {},
-                        'games': [],
-                        'op_score': 0,
-                        'wins_by_turn': Counter(),
-                        'eliminations': Counter(),
-                        'assists': 0
-                    }
-
-            decks[winner_name]['score'] += len(losers)
-
-            for deck_name in players:
-                deck = decks[deck_name]
-                deck['played'] += 1
-                # for op in players:
-                #     if op != deck_name and op not in deck['matchups']:
-                #         deck['matchups'][op] = 0
-
-                if deck_name == winner_name:
-                    deck['wins'] += 1
-
-                    for loser_name in losers:
-                        if loser_name not in deck['matchups']:
-                            deck['matchups'][loser_name] = 0
-
-                        deck['matchups'][loser_name] += 1
-                        if deck_name not in diffs[date]:
-                            diffs[date][deck_name] = {}
-
-                        diffs[date][deck_name][loser_name] = deck['matchups'][loser_name]
-                else:
-                    if winner_name not in deck['matchups']:
-                        deck['matchups'][winner_name] = 0
-                    deck['matchups'][winner_name] -= 1
-
-                    if deck_name not in diffs[date]:
-                        diffs[date][deck_name] = {}
-
-                    diffs[date][deck_name][winner_name] = deck['matchups'][winner_name]
-                    deck['score'] -= 1
-
-                if 'eliminations' in game:
-                    eliminations = {
-                        int(turn): turn_elims
-                        for turn, turn_elims
-                        in game['eliminations'].items()
-                    }
-
-                    if deck_name == winner_name:
-                        turn_won = max(eliminations.keys())
-                        deck['wins_by_turn'][turn_won] += 1
-
-                    for t, e in eliminations.items():
-                        for e_record in e:
-                            if e_record['eliminator'] == deck_name:
-                                deck['eliminations'][t] += len(e_record['eliminated'])
-                                if deck_name != winner_name:
-                                    deck['assists'] += 1
-
-                game_object = {
-                    'date': date,
-                    'players': {
-                        op: (1 if deck_name == winner_name else -1 if op == winner_name else 0) for op in
-                        [p for p in players if p is not deck_name]
-                    }
-                }
-
-                deck['games'].append(game_object)
-
-    for deck in decks.values():
-        deck['op_score'] = sum(map(lambda opr: decks[opr[0]]['score'] * opr[1], deck['matchups'].items()))
-
-    return decks, diffs, wins_by_turn_order
-
-
-def truskrillex(date=None):
-    with open('./data/edh.json', 'r',) as f:
-        record = json.load(f)
-    gs = record['games']
-
-    ratings_by_deck = {}
-    ratings_by_deck_r = {}
-
-    for g in gs:
-        if date and parse_date(g['date']) >= parse_date(date):
-            continue
-        for game in g['games']:
-            archideck_names = {
-                p['deck']: p['deck'] + ' - ' + (p['player'] if 'architect' not in p else p['architect'])
-                for p in game['players']
-            }
-            winner = archideck_names[game['winner']]
-            decks = list(map(lambda p: archideck_names[p['deck']], game['players']))
-            losers = list(filter(lambda dck: dck != winner, decks))
-
-            for d in decks:
-                if d not in ratings_by_deck:
-                    ratings_by_deck[d] = Rating()
-
-            rating_group = [(ratings_by_deck[winner],)] + [(ratings_by_deck[l],) for l in losers]
-            rating_group_r = [(ratings_by_deck[winner],)] + list(reversed([(ratings_by_deck[l],) for l in losers]))
-            ranks = [0] + ([1] * len(losers))
-            new_ratings = rate(rating_group, ranks=ranks)
-            new_ratings_r = rate(rating_group_r, ranks=ranks)
-            test_ratings = rate(rating_group, ranks=list(range(len(decks))))
-            for idx, (rating,) in enumerate(new_ratings):
-                if idx == 0:
-                    ratings_by_deck[winner] = rating
-                else:
-                    ratings_by_deck[losers[idx-1]] = rating
-
-            for idx, (rating,) in enumerate(new_ratings_r):
-                if idx == 0:
-                    ratings_by_deck_r[winner] = rating
-                else:
-                    ratings_by_deck_r[losers[len(losers) - idx]] = rating
-
-    # env = global_env()
-
-    # for d, r in ratings_by_deck.items():
-    #     print(f'{d}\t{env.expose(r)}')
-    # print()
-
-    # for deck, rating in sorted(ratings_by_deck.items(), key=lambda dxr: -dxr[1].mu):
-    #     print(f'{deck}\t{rating.mu}\t{rating.sigma}')
-    #
-    # for deck, rating in sorted(ratings_by_deck_r.items(), key=lambda dxr: -dxr[1].mu):
-    #     print(f'{deck}\t{rating.mu}\t{rating.sigma}')
-
-    sort_by_mu = lambda dxr: -dxr[1].mu
-    alphabetical = lambda dxr: dxr[0]
-
-    # for (deck, rating), (_, rating_r) in zip(sorted(ratings_by_deck.items(), key=alphabetical),
-    #                                          sorted(ratings_by_deck_r.items(), key=alphabetical)):
-    #     print(f'{deck}\t{avg(rating.mu, rating_r.mu)}\t{avg(rating.sigma, rating_r.sigma)}')
-
-    return {
-        deck: (statistics.mean([rating.mu, rating_r.mu]), statistics.mean([rating.sigma, rating_r.sigma]))
-        for (deck, rating), (_, rating_r)
-        in zip(sorted(ratings_by_deck.items(), key=alphabetical),
-               sorted(ratings_by_deck_r.items(), key=alphabetical))
-    }
-
-
 def print_wins(wins):
     for p in [3, 4]:
         print(f'{p}-player games')
@@ -619,178 +437,6 @@ def print_wins(wins):
         for i in range(1, p+1):
             ws = wins[p][i]
             print(f'{i}\t{ws}\t{ws / total}')
-
-
-def print_diffs(decks, diffs):
-    alphabetical = lambda kvp: kvp[0]
-    by_score = lambda kvp: -decks[kvp[0]]['score']
-
-    for date, ds in diffs.items():
-        print(date)
-        for deck, mus in sorted(ds.items(), key=by_score):
-            print(f'{deck}\tvs.')
-            for op, score in sorted(mus.items(), key=lambda oxs: -oxs[1]):
-                print(f'{op}\t{score}')
-            print()
-
-
-def print_games_by_deck(decks):
-    for deck in sorted(decks.values(), key=lambda d: d['name']):
-        print(deck['name'], end='')
-        for game in deck['games']:
-            print('\t' + game['date'])
-            for (op, result) in game['players'].items():
-                print(f'{op}\t{result}')
-        print()
-
-
-def print_matchups(decks, key):
-    for deck in sorted(decks.values(), key=key):
-        print(f"{deck['name']}\tvs.")
-        for op, score in sorted(deck['matchups'].items(), key=lambda kvp: kvp[0]):
-            print(f'{op}\t{score}')
-        print()
-
-
-def print_graph(decks):
-    edges = set()
-    for deck in sorted(decks.values(), key=lambda d: -d['score']):
-        for opponent, score in deck['matchups'].items():
-            if score > 0:
-                edges.add(f"  \"{deck['name']}\" -> \"{opponent}\"")
-    print('digraph G {')
-    for edge in edges:
-        print(edge)
-    print("}")
-
-
-def calculate_scores(decks, trueskill_ratings):
-    for deck in decks.values():
-
-        avg_n_players = statistics.mean(
-            map(lambda game: len(game['players']) + 1,
-                deck['games']
-                )
-        )
-
-        deck_name = deck['name']
-        wins = deck['wins']
-        played = deck['played']
-
-        winrate = wins / played
-        deck['winrate'] = winrate
-
-        expected_winrate = 1 / avg_n_players
-        deck['expected_winrate'] = expected_winrate
-
-        winrate_delta = winrate - expected_winrate
-        deck['winrate_delta'] = winrate_delta
-
-        wins_by_turn = deck['wins_by_turn']
-        if wins == 0:
-            fastest_win = avg_turn_win = 'n/a'
-        elif len(wins_by_turn) == 0:
-            fastest_win = avg_turn_win = '?'
-        else:
-            fastest_win = min(wins_by_turn.keys())
-            avg_turn_win = statistics.mean(wins_by_turn.elements())
-        deck['fastest_win'] = fastest_win
-        deck['avg_turn_win'] = avg_turn_win
-
-        eliminations = deck['eliminations']
-        if len(eliminations) == 0:
-            elims = avg_elim_turn = fastest_elim = assists = '?'
-        else:
-            elims = eliminations.total()
-            avg_elim_turn = statistics.mean(eliminations.elements())
-            fastest_elim = min(eliminations.keys())
-            assists = deck['assists'] if deck['assists'] else ''
-        deck['elims'] = elims
-        deck['fastest_elim'] = fastest_elim
-        deck['avg_elim_turn'] = avg_elim_turn
-        deck['assists'] = assists
-
-        op_wins = deck['op_wins']
-        op_lose = deck['op_lose']
-        op_winrate = op_wins / (op_wins + op_lose)
-        deck['op_winrate'] = op_winrate
-
-        wrx = winrate * op_winrate
-        wrx1 = (1+winrate) * (1+op_winrate)
-        mu, sigma = trueskill_ratings[deck_name]
-
-        deck['wrx1'] = wrx1
-        deck['mu'] = mu
-        deck['sigma'] = sigma
-
-
-def rank_by(decks, rank_name, ranker):
-    rank = 1
-    for deck in sorted(decks, key=ranker):
-        if deck['played'] > 2:
-            deck[rank_name] = rank
-            rank += 1
-        else:
-            deck[rank_name] = ''
-
-
-def calculate_ranks(decks):
-    def by_tbs(d):
-        return -d['winrate'], -d['played'], -d['op_winrate']
-
-    def by_delta(d):
-        return -d['winrate_delta'], -d['played'], -d['op_winrate']
-
-    ranked = decks.values()
-
-    rank_by(ranked, 'by_tbs', by_tbs)
-    rank_by(ranked, 'by_delta', by_delta)
-    rank_by(ranked, 'by_wrx1', lambda d: -d['wrx1'])
-    rank_by(ranked, 'by_mmr', lambda d: -d['mu'])
-
-
-def print_scores(decks, key):
-    print("name\twins\tplayed\twin %\tex. win %\twin % delta\tfastest win\tavg win turn\teliminations"
-          "\tfastest elimination\tavg elim turn\tassists\tscore\top_score\top_wins\top_lose\top win %"
-          "\twrx1 ((win % + 1) x (op win % + 1))\tmmr\tsigma\tBy tiebreaks (win %, played, op win %)"
-          "\tBy tiebreaks (win % delta, played, op win %)\tBy wrx1\tBy mmr")
-    for deck in sorted(decks.values(), key=key):
-        print(f"{deck['name']}"
-              f"\t{deck['wins']}"
-              f"\t{deck['played']}"
-              f"\t{deck['winrate']}"
-              f"\t{deck['expected_winrate']}"
-              f"\t{deck['winrate_delta']}"
-              f"\t{deck['fastest_win']}"
-              f"\t{deck['avg_turn_win']}"
-              f"\t{deck['elims']}"
-              f"\t{deck['fastest_elim']}"
-              f"\t{deck['avg_elim_turn']}"
-              f"\t{deck['assists']}"
-              f"\t{deck['score']}"
-              f"\t{deck['op_score']}"
-              f"\t{deck['op_wins']}"
-              f"\t{deck['op_lose']}"
-              f"\t{deck['op_winrate']}"
-              f"\t{deck['wrx1']}"
-              f"\t{deck['mu']}"
-              f"\t{deck['sigma']}"
-              f"\t{deck['by_tbs']}"
-              f"\t{deck['by_delta']}"
-              f"\t{deck['by_wrx1']}"
-              f"\t{deck['by_mmr']}"
-            )
-
-
-def opponents_wins_losses(decks):
-    for deck in decks.values():
-        opw = 0
-        opl = 0
-        for op in deck['matchups']:
-            opw += decks[op]['wins']
-            opl += decks[op]['played'] - decks[op]['wins']
-        deck['op_wins'] = opw
-        deck['op_lose'] = opl
 
 
 def parse_date(d):
@@ -813,23 +459,6 @@ def main():
     wins = update_record_results(decks, games)
     print_decks(decks, key=alphabetical)
     print_wins(wins)
-
-    # decks, diffs, wins = foo(date)
-    # opponents_wins_losses(decks)
-    # trueskill_ratings = trueskill()
-    # calculate_scores(decks, trueskill_ratings)
-    # calculate_ranks(decks)
-    # print_scores(decks, key=alphabetical)
-    # print_wins(wins)
-
-    # print()
-    # print_graph(decks)
-    # print()
-    # print_matchups(decks, key=lambda d: d['name'])
-    # print()
-    # print_games_by_deck(decks)
-    # print()
-    # print_diffs(decks, diffs)
 
 
 if __name__ == '__main__':
